286009d89574b37545c70cd43617afc2894b4766
Alex Rosa <alex.rosa@ntconsult.com.br>
2018-06-19 18:34:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# default-system
+
+Configuações: 
+- Acesso ao H2:
+
+http://localhost:8080/h2-console
+    -> JDBC URL: jdbc:h2:mem:testdb
---
+++ src/main/java/com/edu/defaultsystem/DefaultSystemApplication.java
@@ -0,0 +1,12 @@
+package com.edu.defaultsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultSystemApplication.class, args);
+	}
+}
